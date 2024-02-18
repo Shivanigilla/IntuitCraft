d20e3681dc5d4d2bf1404ffc3a30a9797d1927c9
Shivani Gilla <shivanigilla@Shivanis-MacBook-Air.local>
2024-02-18 15:33:33 +0530

    Working copy with basic spring security and Rate limiting

---
+++ src/main/java/com/intuit/commentsService/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.intuit.commentsService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("spring")
+                .password(encoder.encode("secret"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
